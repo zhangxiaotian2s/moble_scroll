c148d5d4f2f31ea6cef7f8f5a52f21bfea4d1257
zhangxiaotianmacpor <zhangxiaotian@zhangxiaotian.local>
2015-04-22 17:58:47 +0800

    new

--- js/module.js
+++ js/module.js
@@ -1,47 +1,55 @@
 (function($) {
 	$.fn.mymodule = function(options) {
+
+		var w_height = $(window).height()
+
 		var Option = {
 				allowmove: true,
-				wrap: "#wrap",
-				m_box: ".module",
-				touch_box: ".touch_div", //滑动触摸层
-				height: $(window).height(),
+				c_wrap: "#c_wrap",
+				c_module: ".module",
+				c_touchbox: ".touch_div", //滑动触摸层
 				loadid: "#loading", //初次打开的加载的DIV
 				blur: "blur", //模糊滤镜
-				btn_next: "#btn_next",
-				btn_pre: "#btn_pre",
-				zindex: 5,
-				m_length: 5,
+				c_btnnext: "#btn_next",
+				c_btnpre: "#btn_pre",
+				height: w_height,
+				zindex: 4,
+				m_length: 4,
 				overtime: 200,
 				m_i: 0, //参数
 				m_style: function() { //起始设置
 					var _this = this;
-					$(_this.wrap).css("height", _this.height);
-					$(_this.m_box).css("height", _this.height);
-					_this.m_top(0)
+					$(_this.c_wrap).css("height", _this.height);
+					$(_this.c_module).css("height", _this.height);
+					_this.m_uptop(0)
 					_this.m_hide()
 				},
 				m_resize: function(h) { //变化传至控制高度
 					var _this = this;
-					$(_this.wrap).css("height", h);
-					$(_this.m_box).css("height", h);
+					$(_this.c_wrap).css("height", h);
+					$(_this.c_module).css("height", h);
 				},
 				m_hide: function() {
 					var _this = this;
-					$(_this.m_box).hide()
-					$(_this.m_box).first().show()
+					$(_this.c_module).hide()
+					$(_this.c_module).first().show()
 				},
-				m_top: function(i) { //设置top
+				m_uptop: function(i) { //设置top
 					var _this = this
-					$(_this.m_box).css("top", this.height)
-					$(_this.m_box).eq(i).css("top", 0)
+					$(_this.c_module).css("top", this.height)
+					$(_this.c_module).eq(i).css("top", 0)
+				},
+				m_downtop: function(i) { //设置top
+					var _this = this
+					$(_this.c_module).css("top", 0)
+					$(_this.c_module).eq(i).css("top", -this.height)
 				},
 				m_zindex: function(i, zero) {
 					if (zero != 0) {
-						$(Option.m_box).css("z-index", 0)
-					} else {
 						Option.zindex++
-							$(Option.m_box).eq(i).css("z-index", Option.zindex.zindex)
+							$(Option.c_module).eq(i).css("z-index", Option.zindex)
+					} else {
+						$(Option.c_module).css("z-index", 0)
 					}
 				}
 			}
@@ -59,13 +67,14 @@
 					}
 					Option.m_i = j
 					Option.m_zindex(j)
-					Option.m_top(i)
-					$(Option.m_box).eq(j).show().animate({
-						"top": 0
-					}, Option.overtime)
-					$(Option.m_box).eq(i).slideUp(Option.overtime+100, function() {
+					Option.m_uptop(i)
+					$(Option.c_module).eq(j).show().animate({
+						"top": '0px'
+					}, Option.overtime, 'linear', function() {
+						$(Option.c_module).eq(i).hide(Option.overtime + 100)
 						Option.allowmove = true
 					})
+
 				},
 				m_touchdown: function(i) { //向下滑
 					Option.allowmove = false
@@ -76,12 +85,27 @@
 						j = i - 1
 					}
 					Option.m_i = j
-					Option.m_zindex(j, 0)
-					$(Option.m_box).eq(j).css("top", 0)
-					$(Option.m_box).eq(j).slideDown(Option.overtime, function() {
-						$(Option.m_box).eq(i).hide();
-						Option.allowmove = true;
-					})
+					Option.m_zindex(j)
+					Option.m_downtop(j)
+//					$(Option.c_module).eq(j).css('top', -Option.height)
+					$(Option.c_module).eq(j).show().animate({
+							"top": '0px'
+						}, Option.overtime, 'linear', function() {
+							$(Option.c_module).eq(i).hide();
+							Option.allowmove = true;
+						})
+						//					$(Option.c_module).eq(j).slideDown(Option.overtime, function() {
+						//						$(Option.c_module).eq(i).hide();
+						//						Option.allowmove = true;
+						//					})
+				},
+				zepto_index: function(mythis, ClassName) { //类似JQ  index()效果
+					var arr = document.getElementsByClassName(ClassName)
+					for (i = 0; i < arr.length; i++) {
+						if (mythis == arr[i]) {
+							return i
+						}
+					}
 				}
 			}
 			//load 处理
@@ -90,40 +114,44 @@
 				blur: "blur", //模糊滤镜
 				remove_load: function() {
 					this.loadid.remove();
-					$(Option.m_box).first().removeClass(this.blur);
+					$(Option.c_module).first().removeClass(this.blur);
 				}
 			}
-		//阻止默认的滑动效果
+			//阻止默认的滑动效果
 		$("body").bind('touchmove', function(e) {
 				e.preventDefault()
 			})
 			//向上滑动处理
-		touch.on(Option.touch_box, 'swipeup', function() {
+		touch.on(Option.c_touchbox, 'swipeup', function() {
 				if (Option.allowmove == true) {
-					var i = $(this).index(Option.touch_box);
+					var i = action_touch.zepto_index(this, 'touch_div')
 					action_touch.m_touchup(i)
 				}
 			})
 			//向下滑动处理
-		touch.on(Option.touch_box, 'swipedown', function() {
+		touch.on(Option.c_touchbox, 'swipedown', function() {
 				if (Option.allowmove == true) {
-					var i = $(this).index(Option.touch_box);
+					var i = action_touch.zepto_index(this, 'touch_div')
 					action_touch.m_touchdown(i)
 				}
 			})
 			//click  btn
-		$("#btn_next").click(function() {
+		$(Option.c_btnnext).click(function() {
 			if (Option.allowmove == true) {
 				action_touch.m_touchup(Option.m_i);
 			}
 		})
 
-		$("#btn_pre").click(function() {
+		$(Option.c_btnpre).click(function() {
 				if (Option.allowmove == true) {
 					action_touch.m_touchdown(Option.m_i);
 				}
 			})
-			//加载时候执行
+			//
+
+
+
+		//加载时候执行
 		$(document).ready(function() {
 				Option.m_style()
 			})
@@ -138,4 +166,4 @@
 
 	}
 
-}(jQuery))
+}(Zepto))
